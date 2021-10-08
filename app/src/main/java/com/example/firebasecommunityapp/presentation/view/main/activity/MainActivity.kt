02c5edcd36de9e0545e63ca055b91b5e6d4934ff
yunsung <12345ggh81@gmail.com>
2021-10-08 15:59:09 +0900

    [UPDATE] 기존 코드의 자잘자잘한 변경점

--- app/src/main/java/com/example/firebasecommunityapp/presentation/view/main/activity/MainActivity.kt
+++ app/src/main/java/com/example/firebasecommunityapp/presentation/view/main/activity/MainActivity.kt
@@ -1,12 +1,12 @@
-package com.example.firebasecommunityapp.ui.activity
+package com.example.firebasecommunityapp.presentation.view.main.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.firebasecommunityapp.R
 
-class LoginActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_main)
     }
 }
--- app/src/main/java/com/example/firebasecommunityapp/presentation/view/signup/activity/SignUpActivity.kt
+++ app/src/main/java/com/example/firebasecommunityapp/presentation/view/signup/activity/SignUpActivity.kt
@@ -1,12 +1,14 @@
-package com.example.firebasecommunityapp.ui.activity
+package com.example.firebasecommunityapp.presentation.view.signup.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.firebasecommunityapp.R
 
-class MainActivity : AppCompatActivity() {
+class SignUpActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_signup)
+
     }
 }
