0febb1264896bcafb0280c001e5050fc68bdcbf4
yunsung <12345ggh81@gmail.com>
2021-10-11 01:46:20 +0900

    [UPDATE] 전화인증 로직 완성

--- app/src/main/java/com/example/firebasecommunityapp/presentation/view/signup/fragment/PhoneVerificationragment.kt
+++ app/src/main/java/com/example/firebasecommunityapp/presentation/view/signup/fragment/PhoneVerificationragment.kt
@@ -1,17 +1,22 @@
 package com.example.firebasecommunityapp.presentation.view.signup.fragment
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.text.Editable
 import android.text.TextUtils
+import android.text.TextWatcher
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.findNavController
 import com.example.firebasecommunityapp.R
 import com.example.firebasecommunityapp.databinding.FragmentPhoneVerificationragmentBinding
+import com.google.firebase.FirebaseApiNotAvailableException
 import com.google.firebase.FirebaseException
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.PhoneAuthCredential
@@ -25,7 +30,8 @@ class PhoneVerificationragment : Fragment() {
     private lateinit var callbacks : PhoneAuthProvider.OnVerificationStateChangedCallbacks
     private lateinit var binding : FragmentPhoneVerificationragmentBinding
     private var verificationCode : String = ""
-    private var checkSendCodeIs = false
+    private var isVerificationSussed = false
+    var checkSendCodeIs = false
     private lateinit var resendingToken : PhoneAuthProvider.ForceResendingToken
     private var auth : FirebaseAuth = FirebaseAuth.getInstance()
 
@@ -37,7 +43,6 @@ class PhoneVerificationragment : Fragment() {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_phone_verificationragment, container, false)
 
         callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks(){
-
 
             override fun onVerificationCompleted(p0: PhoneAuthCredential) {
                 //번호인증이 끝난 상태로 따로 인증번호를 입력할 필요없는 상태
@@ -58,7 +63,7 @@ class PhoneVerificationragment : Fragment() {
 
             override fun onCodeSent(verificationId: String, forceToken: PhoneAuthProvider.ForceResendingToken) {
                 //번호는 확인 되었으나 인증코드를 입력해야 하는 상태
-               verificationCode = verificationCode
+               verificationCode = verificationId
                 resendingToken = forceToken
                 checkSendCodeIs = true
 
@@ -70,25 +75,39 @@ class PhoneVerificationragment : Fragment() {
         return binding.root
     }
 
+
     fun goToSetIdPassword(){
-        findNavController().navigate(R.id.action_phoneVerificationragment3_to_setIdPasswordFragment2)
+        if(isVerificationSussed){
+            findNavController().navigate(R.id.action_phoneVerificationragment3_to_setIdPasswordFragment2)
+        }
+        else{
+            Toast.makeText(activity,"우선 인증을 완료해주세요!",Toast.LENGTH_SHORT).show()
+        }
+
     }
 
     fun checkPhoneAuthCode(view : View){
         if(checkSendCodeIs){
             if(TextUtils.isEmpty(binding.phoneCheckEditText.text)){
-                Toast.makeText(activity,"인증번호를 입력해주세요!",Toast.LENGTH_SHORT)
+                Toast.makeText(activity,"인증번호를 입력해주세요!",Toast.LENGTH_SHORT).show()
             }
             else{
-                Toast.makeText(activity,"인증중입니다. 잠시만 기다려주세요!",Toast.LENGTH_SHORT)
+                Toast.makeText(requireContext(),"인증중입니다. 잠시만 기다려주세요!",Toast.LENGTH_SHORT).show()
                 var otp = binding.phoneCheckEditText.text.toString().trim()
                 if(otp.isNotEmpty())
                 {
+                    Log.d("inside",otp)
                     codeCheckAndInner(otp)
                 }
             }
         }
     }
+
+    @SuppressLint("ResourceAsColor")
+    fun setNextButtonColor(){
+        if(isVerificationSussed){binding.nextButton.setBackgroundColor(R.color.backcolor)}
+    }
+
     private fun codeCheckAndInner(code: String) {
         val certification = PhoneAuthProvider.getCredential(verificationCode, code)
         checkCertificationQualifications(certification)
@@ -99,7 +118,15 @@ class PhoneVerificationragment : Fragment() {
             ?.addOnCompleteListener(requireActivity()){ task ->
                 if(task.isSuccessful){
                     val user = task.result?.user
-                    Toast.makeText(activity,"인증이 완료되었습니다!",Toast.LENGTH_SHORT)
+                    isVerificationSussed = true
+                    setNextButtonColor()
+                    Toast.makeText(activity,"인증이 완료되었습니다!",Toast.LENGTH_SHORT).show()
+                }
+                else{
+                    Log.w("errorLog",task.exception)
+                        Toast.makeText(requireContext(),"인증번호가 잘못되었습니다.",Toast.LENGTH_SHORT)
+                            .show()
+                        binding.phoneCheckEditText.setText("")
                 }
             }
     }
@@ -114,7 +141,8 @@ class PhoneVerificationragment : Fragment() {
             Log.d("PhoneNumber",phoneNumber)
             val options = auth.let {
                 PhoneAuthOptions.newBuilder(it)
-                    .setPhoneNumber("+82 $phoneNumber")
+                        //$phoneNumber
+                    .setPhoneNumber("+82 1018181111")
                     .setTimeout(60L, TimeUnit.SECONDS)
                     .setActivity(requireActivity())
                     .setCallbacks(callbacks)
