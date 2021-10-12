199861f2493e25c0fe62edd0545b566b132225cf
yunsung <12345ggh81@gmail.com>
2021-10-12 21:15:07 +0900

    [CREATE] di 추가

---
+++ app/src/main/java/com/example/firebasecommunityapp/presentation/wiget/JabchwiApplication.kt
@@ -0,0 +1,11 @@
+package com.example.firebasecommunityapp.presentation.wiget
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class JabchwiApplication : Application(){
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
