796e0c987aa9719c9afa1aa86534a817362a35a5
yunsung <12345ggh81@gmail.com>
2021-10-13 20:27:48 +0900

    [CREATE] data Class 추가

---
+++ app/src/main/java/com/example/firebasecommunityapp/data/model/SingUp.kt
@@ -0,0 +1,13 @@
+package com.example.firebasecommunityapp.data.model
+
+
+object SingUp {
+
+    var phoneNumber : String? = null
+    var nickName : String? = null
+    var password : String? = null
+    var id : String? = null
+    var profilePicture : String? = null
+
+}
+
---
+++ app/src/main/java/com/example/firebasecommunityapp/presentation/wiget/utils/UserProfile.kt
@@ -0,0 +1,11 @@
+package com.example.firebasecommunityapp.presentation.wiget.utils
+
+import android.net.Uri
+
+object UserProfile{
+    var phoneNumber : String? = null
+    var nickName : String? = null
+    var password : String? = null
+    var id : String? = null
+    var profilePicture : Uri? = null
+}
