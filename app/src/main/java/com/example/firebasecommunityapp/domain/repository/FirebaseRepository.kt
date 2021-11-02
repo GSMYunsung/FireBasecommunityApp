6784cc4ec90ae33ccd97da02cc17d5ef69c8ee73
choiyunsung <12345ggh81@gmail.com>
2021-11-02 20:48:16 +0900

    [UPDATE] 유저의 프로필사진과 닉네임 설정 로직 구현

--- app/src/main/java/com/example/firebasecommunityapp/domain/repository/FirebaseRepository.kt
+++ app/src/main/java/com/example/firebasecommunityapp/domain/repository/FirebaseRepository.kt
@@ -1,10 +1,12 @@
 package com.example.firebasecommunityapp.domain.repository
 
 import com.google.firebase.database.DatabaseReference
+import com.google.firebase.firestore.CollectionReference
 import dagger.Provides
 
 interface FirebaseRepository {
     //데이터베이스를 읽거나 쓸 수있다.
     fun callUserInfo() : DatabaseReference
     fun idUserInfo(id : String) : DatabaseReference
+    fun nicknameInfo(nickname : String) : CollectionReference
 }
