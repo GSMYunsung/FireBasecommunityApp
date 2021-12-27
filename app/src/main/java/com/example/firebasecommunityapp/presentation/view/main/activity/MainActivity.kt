b2ea3839af41c3ade7e61c73ece0723d9038d5d7
choiyunsung <12345ggh81@gmail.com>
2021-12-27 23:07:52 +0900

    [UPDATE] 바텀네비게이션 추가

--- app/src/main/java/com/example/firebasecommunityapp/presentation/view/main/activity/MainActivity.kt
+++ app/src/main/java/com/example/firebasecommunityapp/presentation/view/main/activity/MainActivity.kt
@@ -5,16 +5,49 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.LiveData
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.firebasecommunityapp.R
 import com.example.firebasecommunityapp.databinding.ActivityIntroBinding
 import com.example.firebasecommunityapp.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    lateinit var navi: BottomNavigationView
+    private lateinit var navController: NavController
     private lateinit var binding : ActivityMainBinding
     var mBackWait:Long = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        navi = binding.bottomNav
+        navController = findNavController(R.id.fragment)
+        //앱 바 구성성
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.mainFragment,
+                R.id.shoppingFragment,
+                R.id.communityFragment,
+                R.id.mapFragment,
+                R.id.profileFragment
+                )
+        )
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navi.setupWithNavController(navController)
+        initNavigation()
+    }
 
     override fun onBackPressed() {
 
@@ -29,8 +62,23 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+    private fun initNavigation() {
+        val navController = findNavController(R.id.fragment)
+        binding.bottomNav.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if (destination.id == R.id.mainFragment || destination.id == R.id.shoppingFragment || destination.id == R.id.communityFragment || destination.id == R.id.mapFragment || destination.id == R.id.profileFragment) {
+                binding.bottomNav.visibility = View.VISIBLE
+            } else {
+                binding.bottomNav.visibility = View.GONE
+            }
+
+
+        }
     }
 }
