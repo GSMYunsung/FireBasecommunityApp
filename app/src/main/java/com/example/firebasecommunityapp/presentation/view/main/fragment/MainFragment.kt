b3f4556aefa8523b25e992ff601d66dea9d4ca6b
choiyunsung <12345ggh81@gmail.com>
2022-01-10 18:19:15 +0900

    [UPDATE] main 화면 ui 수정에 따른 수정

--- app/src/main/java/com/example/firebasecommunityapp/presentation/view/main/fragment/MainFragment.kt
+++ app/src/main/java/com/example/firebasecommunityapp/presentation/view/main/fragment/MainFragment.kt
@@ -1,6 +1,5 @@
 package com.example.firebasecommunityapp.presentation.view.main.fragment
 
-import android.gesture.GestureOverlayView.ORIENTATION_HORIZONTAL
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,12 +7,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import androidx.viewpager2.widget.ViewPager2
 import com.example.firebasecommunityapp.R
-import com.example.firebasecommunityapp.databinding.ActivityMainBinding
 import com.example.firebasecommunityapp.databinding.FragmentMainBinding
-import com.example.firebasecommunityapp.presentation.view.main.adapter.MainCatagoryAdapter
 
 
 class MainFragment : Fragment() {
@@ -27,14 +22,7 @@ class MainFragment : Fragment() {
 
         binding =  DataBindingUtil.inflate(inflater, R.layout.fragment_main, container, false)
 
-        binding.mainCatagoryViewpager.layoutManager = LinearLayoutManager(requireContext(),LinearLayoutManager.HORIZONTAL,false)
-        binding.mainCatagoryViewpager.setHasFixedSize(true)
-        binding.mainCatagoryViewpager.adapter = MainCatagoryAdapter(setCatagory())
-
         return binding.root
     }
 
-    private fun setCatagory(): ArrayList<String> {
-        return arrayListOf("All", "Food","Join","Myself")
-    }
 }
